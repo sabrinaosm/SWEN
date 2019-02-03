81729628f2a86f79fd4b498d3b8518764e9bf3e9
1702107B <46706823+1702107B@users.noreply.github.com>
2019-02-03 16:47:24 +0800

    Update delonixstaff.service.spec.ts

--- src/app/Housekeeping/delonixstaff.service.spec.ts
+++ src/app/Housekeeping/delonixstaff.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { DelonixstaffService } from './delonixstaff.service';
+import { DelonixstaffService } from './Housekeeping/delonixstaff.service';
 
 describe('DelonixstaffService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));

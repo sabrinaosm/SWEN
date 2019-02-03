aae59e5408d1212c432cd87fc1019960e99f5a27
1702107B <46706823+1702107B@users.noreply.github.com>
2019-02-03 16:31:30 +0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { StaffComponent } from './staff/staff.component';
+import { HomeComponent } from './home/home.component';
+import { routing } from './app.routing';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { DelonixstaffService } from './delonixstaff.service';
+import { StaffPipe } from './staff.pipe';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StaffComponent,
+    HomeComponent,
+    StaffPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [DelonixstaffService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

e68f49b1c434aba4af12759d6ab9f238b8e0e524
m4tte <47273211+m4tte@users.noreply.github.com>
2019-02-03 16:41:09 +0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+// ROOT
 import { AppComponent } from './app.component';
+
+// HOUSEKEEPING COMPONENTS
 import { StaffComponent } from './staff/staff.component';
 import { HomeComponent } from './home/home.component';
+
+// REPORT COMPONENTS
+import { OccupancyComponent } from './Report/occupancy/occupancy.component';
+import { StockComponent } from './Report/stock/stock.component';
+
+// ADDITIONAL MODULES
 import { routing } from './app.routing';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
-import { DelonixstaffService } from './delonixstaff.service';
-import { StaffPipe } from './staff.pipe';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+// SERVICES
+import { ReportService} from './Report/report.service';
+import { DelonixstaffService } from './delonixstaff.service';
+
+// PIPES
+import { StaffPipe } from './staff.pipe';
+import { YearPipe } from './Pipes/Report/year.pipe';
+import { MonthPipe} from './Pipes/Report/month.pipe';
+import { ItemPipe } from './Pipes/Report/item.pipe';
+import { CategoryPipe } from './Pipes/Report/category.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     StaffComponent,
     HomeComponent,
-    StaffPipe
+    OccupancyComponent,
+    StockComponent,
+    StaffPipe,
+    YearPipe,
+    MonthPipe,
+    ItemPipe,
+    CategoryPipe
   ],
   imports: [
     BrowserModule,
@@ -26,7 +52,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     NgbModule,
     FormsModule
   ],
-  providers: [DelonixstaffService],
+  providers: [DelonixstaffService, ReportsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

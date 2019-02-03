5f436f4f399c0ca44d7ff8b9812b6ccc8b26473c
1702107B <46706823+1702107B@users.noreply.github.com>
2019-02-03 17:25:39 +0800

    Update app.routing.ts

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,9 +2,13 @@ import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {OccupancyComponent} from './Report/occupancy/occupancy.component';
 import {StockComponent} from './Report/stock/stock.component';
+import { HomeComponent } from './Housekeeping/home/home.component';
+import { StaffComponent } from './Housekeeping/staff/staff.component';
 
 const appRoutes: Routes = [
 { path: 'occupancy', component:OccupancyComponent},
-{ path: 'stock', component:StockComponent}
+{ path: 'stock', component:StockComponent},
+{ path: 'home', component: HomeComponent },
+{ path: 'staff', component: StaffComponent }
 ];
 export const routing:ModuleWithProviders = RouterModule.forRoot(appRoutes);
